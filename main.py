a90197ac5e17f787aff4cef2a4ec0f6ff08aca56
TxChar <pontogreen1@gmail.com>
2024-10-06 01:12:16 +0700

    refactor: 💡 seperate main app and workout function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import cv2
-import numpy as np
 import mediapipe as mp
 import pandas as pd
 import time
 from sqlalchemy import text
 from streamlit_apexjs import st_apexcharts
 import datetime
+from workout import track_bicep_curl  # Import the Bicep Curl function
 
 st.set_page_config(
     page_title="Exercise Tracking",
@@ -14,29 +14,9 @@ st.set_page_config(
     layout="wide",
 )
 
-
 @st.cache_resource
 def load_model():
     return mp.solutions.pose
-
-
-def calculate_angle(a: np.ndarray, b: np.ndarray, c: np.ndarray) -> float:
-    """Calculate the angle between three points a, b, c"""
-    a, b, c = np.array(a), np.array(b), np.array(c)
-
-    # Calculate the angle
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(np.degrees(radians))
-
-    return 360 - angle if angle > 180 else angle
-
-
-def draw_progress_bar(frame: np.ndarray, angle_percentage: float) -> None:
-    """Draws a progress bar on the frame based on the angle percentage."""
-    progress_bar_width = 280
-    cv2.rectangle(frame, (10, 100), (10 + progress_bar_width, 130), (255, 255, 255), -1)  # Background bar
-    progress_bar_length = int(angle_percentage / 100 * progress_bar_width)
-    cv2.rectangle(frame, (10, 100), (10 + progress_bar_length, 130), (0, 255, 0), -1)  # Progress bar
 
 # @st.fragment(run_every="2s")
 def get_task_names(status, date):
@@ -44,68 +24,6 @@ def get_task_names(status, date):
     query_sql = conn.query(f"SELECT * FROM tasks WHERE status = {status} AND DATE(assign_date) = '{date}';", ttl=0)
     return conn.query(f"SELECT * FROM tasks WHERE status = {status} AND DATE(assign_date) = '{date}';", ttl=0)
 
-
-def track_bicep_curl(exercise, target_reps, pose, mp_pose):
-    curl_rep = 0
-    counting = False
-    start_time = time.time()
-    video_placeholder = st.empty()
-
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            st.error("Failed to capture image.")
-            break
-
-        frame = cv2.flip(frame, 1)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Process the image and get the pose landmarks
-        results = pose.process(frame)
-
-        if exercise and results.pose_landmarks:
-            # Draw landmarks
-            mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-            # Get key points
-            landmarks = results.pose_landmarks.landmark
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-
-            # Calculate the angle
-            angle = calculate_angle(shoulder, elbow, wrist)
-
-            # Calculate the percentage for the progress bar (30 to 90 degrees)
-            angle_percentage = 100 if angle < 30 else (0 if angle > 90 else (90 - angle) / 60 * 100)
-
-            # Count curls
-            if angle < 30 and counting:
-                curl_rep += 1
-                counting = False
-            elif angle > 80:
-                counting = True
-
-            # Display the angle
-            cv2.putText(frame, f'Angle: {int(angle)}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-            # Draw progress bar
-            draw_progress_bar(frame, angle_percentage)
-
-            # Display curl count
-            cv2.putText(frame, f'Curls: {curl_rep}/{target_reps}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-            cv2.putText(frame, f'Time: {time.time() - start_time}', (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-            # Finish Task
-            if curl_rep >= target_reps:
-                st.session_state.toast_message = 'Task is done! ✅'
-                return True  # Task is completed
-
-        video_placeholder.image(frame, channels="RGB")
-
-    return False  # Task is not completed
 
 def main():
     st.title("Exercise Tracking")
@@ -124,7 +42,7 @@ def main():
         counting = False
 
         today = datetime.datetime.today().date()
-        # today = "2024-10-02 23:00:00"
+        # today = "2024-10-02 00:00:00"
         df = get_task_names(0, today)
         tasks = {}
         tasks_name = []
@@ -133,13 +51,10 @@ def main():
             task_name = f"{row.exercise} - {row.reps} Reps"
             tasks_name.append(task_name)
             tasks[row.task_id] = task_name
-            
 
         # Initialize MediaPipe Pose
         mp_pose = load_model()
         pose = mp_pose.Pose(static_image_mode=False, model_complexity=1, enable_segmentation=False, smooth_landmarks=True)
-
-
 
         with st.container(height=250):
             st.markdown(
@@ -156,11 +71,9 @@ def main():
                 """
             )
         
-        
         col1, col2, col3 = st.columns(3)
         
         with col1.container(height=550):
-            # time_period = st.selectbox("Time Period", ["Day", "Week", "Month"], placeholder="Select Time Period")
             d = st.date_input("Select Date", datetime.date.today())
             chart_data = get_task_names(1, d)
             chart_data['date'] = pd.to_datetime(chart_data['assign_date']).dt.date
@@ -174,9 +87,7 @@ def main():
                         "show": False
                     }
                 },
-
-                "labels": result.columns.to_list()
-                ,
+                "labels": result.columns.to_list(),
                 "legend": {
                     "show": True,
                     "position": "right",
@@ -210,7 +121,7 @@ def main():
                 ```        
                 """)
             
-        # Use the function inside the condition
+        # Track Bicep Curl
         if exercise and "Bicep Curl" in exercise and start_button:
             task_completed = track_bicep_curl(exercise, target_reps, pose, mp_pose)
             if task_completed:
@@ -230,7 +141,6 @@ def main():
                 st.rerun()
 
     with tab2:
-        
         st.markdown(
             """ 
             ```
---
+++ workout.py
@@ -0,0 +1,89 @@
+# work_out.py
+import cv2
+import time
+import numpy as np
+import mediapipe as mp
+import streamlit as st
+from sqlalchemy import text
+
+def calculate_angle(a: np.ndarray, b: np.ndarray, c: np.ndarray) -> float:
+    """Calculate the angle between three points a, b, c"""
+    a, b, c = np.array(a), np.array(b), np.array(c)
+
+    # Calculate the angle
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(np.degrees(radians))
+
+    return 360 - angle if angle > 180 else angle
+
+
+def draw_progress_bar(frame: np.ndarray, angle_percentage: float) -> None:
+    """Draws a progress bar on the frame based on the angle percentage."""
+    progress_bar_width = 280
+    cv2.rectangle(frame, (10, 100), (10 + progress_bar_width, 130), (255, 255, 255), -1)  # Background bar
+    progress_bar_length = int(angle_percentage / 100 * progress_bar_width)
+    cv2.rectangle(frame, (10, 100), (10 + progress_bar_length, 130), (0, 255, 0), -1)  # Progress bar
+
+
+def track_bicep_curl(exercise, target_reps, pose, mp_pose):
+    """Track Bicep Curl using MediaPipe pose estimation"""
+    curl_rep = 0
+    counting = False
+    start_time = time.time()
+    video_placeholder = st.empty()
+
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.error("Failed to capture image.")
+            break
+
+        frame = cv2.flip(frame, 1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the image and get the pose landmarks
+        results = pose.process(frame)
+
+        if exercise and results.pose_landmarks:
+            # Draw landmarks
+            mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+            # Get key points
+            landmarks = results.pose_landmarks.landmark
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            # Calculate the angle
+            angle = calculate_angle(shoulder, elbow, wrist)
+
+            # Calculate the percentage for the progress bar (30 to 90 degrees)
+            angle_percentage = 100 if angle < 30 else (0 if angle > 90 else (90 - angle) / 60 * 100)
+
+            # Count curls
+            if angle < 30 and counting:
+                curl_rep += 1
+                counting = False
+            elif angle > 80:
+                counting = True
+
+            # Display the angle
+            cv2.putText(frame, f'Angle: {int(angle)}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            # Draw progress bar
+            draw_progress_bar(frame, angle_percentage)
+
+            # Display curl count
+            cv2.putText(frame, f'Curls: {curl_rep}/{target_reps}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            cv2.putText(frame, f'Time: {time.time() - start_time}', (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            # Finish Task
+            if curl_rep >= target_reps:
+                st.session_state.toast_message = 'Task is done! ✅'
+                return True  # Task is completed
+
+        video_placeholder.image(frame, channels="RGB")
+
+    return False  # Task is not completed
